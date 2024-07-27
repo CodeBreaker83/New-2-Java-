fcec12036ea841dc754f85a6905d66864cccda85
CodeBreaker83 <tek.kumar83@gmail.com>
2024-07-27 12:09:30 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,18 @@
+/*Program Name : Sample.Java 
+Objective : Displays Welcome message
+Aurthor : Saravanan
+*/
+
+class Sample1
+{
+  public static void main (String args[])
+   {
+     System.out.println("Welcome to java program");
+   }
+
+}
+
+
+
+
+
