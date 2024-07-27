e94758ca3b24dd89f317ccfc2c055eda25309f0f
CodeBreaker83 <tek.kumar83@gmail.com>
2024-07-27 12:11:48 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,10 @@
+class Test2
+{
+  public static void main (String args[])
+  {
+    System.out.print("Full name - ");
+    System.out.print("Saravana ");
+    System.out.print("Chandra ");
+    System.out.print("kumar");
+  }
+}
