77deb9409af79016b6319dd239194f98be02ed0b
CodeBreaker83 <tek.kumar83@gmail.com>
2024-08-10 21:59:59 +0530

    Add files via upload

---
+++ passfail.java
@@ -0,0 +1,20 @@
+//Java program to print pass or fail
+
+class passfail
+{
+   public static void main(String[]args)
+   {
+        double avg=40;
+	
+     	if(avg>=35)
+			
+		{
+		  System.out.println("Passed...");
+		}
+		else
+		{	
+		  System.out.println("Failed...");
+        }
+		
+   }
+}
